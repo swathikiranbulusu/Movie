7c8c829e68ddd7af1ceb80dbddf90166402024de
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 19:16:46 +0530

    Update movieRoutes.js

--- movie-service/routes/movieRoutes.js
+++ movie-service/routes/movieRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const { addMovie, getMovies } = require('../controllers/movieController');
 const router = express.Router();
+const {
+  getMovies,
+  getMovieById,
+  createMovie,
+  updateMovie,
+  deleteMovie
+} = require('../controllers/movieController');
 
-router.post('/', addMovie);
 router.get('/', getMovies);
+router.get('/:id', getMovieById);
+router.post('/', createMovie);
+router.put('/:id', updateMovie);
+router.delete('/:id', deleteMovie);
 
 module.exports = router;
