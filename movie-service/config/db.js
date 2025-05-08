75bbfad58a2fc859480a1c4dbfa88a3ea7b73510
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 19:16:09 +0530

    Update db.js

--- movie-service/config/db.js
+++ movie-service/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/service-db');
-    console.log('MongoDB connected');
+    await mongoose.connect('mongodb://localhost:27017/movieservice');
+    console.log('MongoDB connected 🎬');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
