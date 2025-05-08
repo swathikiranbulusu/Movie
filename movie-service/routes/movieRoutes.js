a9646f4909617f92cb240aba455edf18945a25ac
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 19:28:38 +0530

    Update movieRoutes.js

--- movie-service/routes/movieRoutes.js
+++ movie-service/routes/movieRoutes.js
@@ -8,9 +8,11 @@ const {
   deleteMovie
 } = require('../controllers/movieController');
 
+console.log('✅ movieRoutes loaded');
+
 router.get('/', getMovies);
 router.get('/:id', getMovieById);
-router.post('/', createMovie);
+router.post('/', createMovie); // ✅ Only one POST route here
 router.put('/:id', updateMovie);
 router.delete('/:id', deleteMovie);
 
