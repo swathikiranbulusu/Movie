1ec7bf0e44a48b9e67957df710f9c864231396ef
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 19:15:44 +0530

    Update server.js

--- movie-service/server.js
+++ movie-service/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const routes = require('./routes');
+const movieRoutes = require('./routes/movieRoutes');
 
 const app = express();
 connectDB();
+
 app.use(express.json());
-app.use('/api', routes);
+app.get('/', (req, res) => res.send('Movie Service is running'));
+app.use('/api/movies', movieRoutes);
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Service running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Movie Service running on port ${PORT}`));
