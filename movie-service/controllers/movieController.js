b1394a9111f6551e0eee27d2b0ac1ed21ee0a430
swathikiranbulusu <146068786+swathikiranbulusu@users.noreply.github.com>
2025-05-08 19:17:15 +0530

    Update movieController.js

--- movie-service/controllers/movieController.js
+++ movie-service/controllers/movieController.js
@@ -1,12 +1,30 @@
 const Movie = require('../models/Movie');
 
-exports.addMovie = async (req, res) => {
-  const movie = new Movie(req.body);
-  await movie.save();
-  res.json(movie);
-};
-
 exports.getMovies = async (req, res) => {
   const movies = await Movie.find();
   res.json(movies);
+};
+
+exports.getMovieById = async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+  if (!movie) return res.status(404).json({ message: 'Movie not found' });
+  res.json(movie);
+};
+
+exports.createMovie = async (req, res) => {
+  const movie = new Movie(req.body);
+  await movie.save();
+  res.status(201).json(movie);
+};
+
+exports.updateMovie = async (req, res) => {
+  const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  if (!movie) return res.status(404).json({ message: 'Movie not found' });
+  res.json(movie);
+};
+
+exports.deleteMovie = async (req, res) => {
+  const movie = await Movie.findByIdAndDelete(req.params.id);
+  if (!movie) return res.status(404).json({ message: 'Movie not found' });
+  res.json({ message: 'Movie deleted' });
 };
